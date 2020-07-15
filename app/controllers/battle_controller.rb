b4bbdf85d1d87d5d07ffb8eae9d3d6dddfa7d29c
ocalvario <oswald.calvario@gmail.com>
2020-07-14 22:33:47 -0400

    Adds initial battle methods + configures routes

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -4,4 +4,33 @@ class API::V1::BattleController < ApplicationController
 
         render json: @battles, status: 200
     end
+
+    def show
+        @battle = Battle.find(params[:id])
+        render json: @battle, status: 200
+    end
+    
+    def create
+        @battle = Battle.create(battle_params)
+
+        render json: @battle, status:200
+    end
+
+    def update
+        @battle = Battle.find(params[:id])
+        @battle.update(battle_params)
+        render json: @battle, status:200
+    end
+
+    def destroy
+        @battle = Battle.find(params[:id])
+        battle.delete
+        render json: {battleID: @battle.id}
+    end
+
+    private
+        def battle_params
+            params.require(:battle).permit(:year)
+        end
+
 end
